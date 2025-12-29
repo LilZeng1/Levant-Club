6b791ed8fbd05b43ed6bf8670e181147daff920f
George Arshed <erdemerdem11221154@gmail.com>
2025-12-29 22:52:00 +0300

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,20 +1,38 @@
+// Variables
+const langToggle = document.getElementById("lang-toggle");
+const btnEn = document.getElementById("btn-en");
+const btnAr = document.getElementById("btn-ar");
+const translatableElements = document.querySelectorAll(".translate");
+
+langToggle.addEventListener("change", () => {
+    const isArabic = langToggle.checked;
+
+    if (isArabic) {
+        btnEn.classList.remove("active");
+        btnAr.classList.add("active");
+        document.body.classList.add("rtl");
+    } else {
+        btnEn.classList.add("active");
+        btnAr.classList.remove("active");
+        document.body.classList.remove("rtl");
+    }
+
+    translatableElements.forEach(el => {
+        el.innerText = isArabic ? el.getAttribute("data-ar") : el.getAttribute("data-en");
+    });
+});
+
+// LastScroll()
 let lastScroll = 0;
 const nav = document.querySelector(".navbar");
 
 window.addEventListener("scroll", () => {
     const current = window.scrollY;
-
     if (current > lastScroll && current > 100) {
-        nav.style.transform = "translate(-50%, -150%)";
+        nav.style.transform = "translate(-50%, -200%)";
     } else {
-        nav.style.transform = "translate(-50%, 0)"; // Göster
+        nav.style.transform = "translate(-50%, 0)";
     }
-
-    if (current <= 20) {
-        nav.style.background = "rgba(3, 7, 18, 0.6)";
-    } else {
-        nav.style.background = "rgba(3, 7, 18, 0.9)";
-    }
-
+    nav.style.background = current <= 20 ? "rgba(3, 7, 18, 0.6)" : "rgba(3, 7, 18, 0.95)";
     lastScroll = current;
 });
