d72c4714d06fb13e3d6948da0b50546d2fc00c2d
George Arshed <erdemerdem11221154@gmail.com>
2026-01-01 05:42:34 +0300

    Update bot.js

--- backend/bot.js
+++ backend/bot.js
@@ -3,7 +3,15 @@ const { Client, GatewayIntentBits } = require("discord.js");
 
 // Client
 const client = new Client({
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers]
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers
+    ]
+});
+
+// CLient OnReady()
+client.once("ready", () => {
+    console.log("Bot is online");
 });
 
 client.login(process.env.BOT_TOKEN);
